f5a827865ce8f1ea42739e0e58f21c412dd5b34d
Paul <sirdjpavline@yandex.ru>
2017-03-22 16:09:46 +0300

    First Commit
    2nd task. Generating playing field by using multiple floor tile prefabs

---
+++ Bomberman 3D/Assets/Scripts/TileView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileView : MonoBehaviour {
+
+    public GameObject tile;
+    public int width;
+    public int height;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                GameObject tileClone = (GameObject)Instantiate(tile, new Vector3(j, 0, i), tile.transform.rotation);
+                tileClone.SetActive(true);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
