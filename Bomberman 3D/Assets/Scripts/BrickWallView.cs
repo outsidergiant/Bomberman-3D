710795283745187d16893d3ee537f1e4156b1954
Paul <sirdjpavline@yandex.ru>
2017-03-22 21:00:24 +0300

    Refactored BWV

--- Bomberman 3D/Assets/Scripts/BrickWallView.cs
+++ Bomberman 3D/Assets/Scripts/BrickWallView.cs
@@ -7,22 +7,30 @@ public class BrickWallView : MonoBehaviour {
     public GameObject brickWall;
     public int numberOfWalls;
     public bool bob;
+
+    private bool[,] wallMap;
 	// Use this for initialization
 	void Start () {
-        bool[,] wallMap = new bool[GlobalParameters.numberTilesX, GlobalParameters.numberTilesZ];
+        wallMap = new bool[GlobalParameters.numberTilesX, GlobalParameters.numberTilesZ];
+        PlaceWalls();
+	}
+
+    void PlaceWalls()
+    {
+         
         for (int i = 0; i < numberOfWalls; i++)
         {
             int x = -1;
             int z = -1;
             do
             {
-                x = (int)Random.RandomRange(1, GlobalParameters.numberTilesX - 1);
-                z = (int)Random.RandomRange(1, GlobalParameters.numberTilesZ - 1);
+                x = (int)Random.Range(1, GlobalParameters.numberTilesX - 1);
+                z = (int)Random.Range(1, GlobalParameters.numberTilesZ - 1);
             } while (x % 2 == 0 && z % 2 == 0 && !wallMap[x, z]);
             wallMap[x, z] = true;
             Clone(x, z);
         }
-	}
+    }
 
     void Clone(int x, int z)
     {
