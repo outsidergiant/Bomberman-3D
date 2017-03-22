6153cf6f100a6de6f3c0766a60b34851094410f0
Paul <sirdjpavline@yandex.ru>
2017-03-22 17:13:14 +0300

    Added inner concrete walls

--- Bomberman 3D/Assets/Scripts/BorderWallView.cs
+++ Bomberman 3D/Assets/Scripts/BorderWallView.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 public class BorderWallView : MonoBehaviour {
 
     public GameObject borderWallTile;
-    //public int width;
-    //public int height;
+    public int tileNumberX;
+    public int tileNumberZ;
 
-    private TileView tileView;
     // Use this for initialization
     void Start()
     {
-        tileView = new TileView();
-        int tileNumberX = 10;//= tileView.numberOfTilesX;
-        int tileNumberZ = 10;//= tileView.numberOfTilesZ;
-
         for (int i = 0; i <= tileNumberX; i++)
         {
             GameObject wallTileClone = (GameObject)Instantiate(borderWallTile, new Vector3(i, 0, 0), borderWallTile.transform.rotation);
@@ -36,10 +31,42 @@ public class BorderWallView : MonoBehaviour {
             GameObject wallTileClone = (GameObject)Instantiate(borderWallTile, new Vector3(0, 0, i), borderWallTile.transform.rotation);
             borderWallTile.SetActive(true);
         }
+
+        AddInnerConcreteWalls();
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    void AddInnerConcreteWalls()
+    {
+        for (int i = 2; i < tileNumberZ; i += 2)
+        {
+            for (int j = 2; j < tileNumberX; j += 2)
+            {
+                GameObject wallTileClone = (GameObject)Instantiate(borderWallTile, new Vector3(j, 0, i), borderWallTile.transform.rotation);
+                borderWallTile.SetActive(true);
+            }
+        }
+    }
+    /*void AddInnerWallTiles()
+    {
+        int numberOfWallTiles = (int)(tileNumberX * tileNumberZ * 0.2); //because I want so.
+        bool[,] wallMap = new bool[tileNumberX, tileNumberZ];
+        for (int i = 0; i < numberOfWallTiles; i++)
+        {
+            int x = -1;
+            int z = -1;
+            do
+            {
+                x = (int)Random.RandomRange(2, tileNumberX - 2);
+                z = (int)Random.RandomRange(2, tileNumberZ - 2);
+            } while (!(wallMap[x, z] && wallMap[x - 1, z] && wallMap[x + 1, z] && wallMap[x, z + 1] && wallMap[x, z - 1] && wallMap[x + 1, z + 1]
+                && wallMap[x + 1, z - 1] && wallMap[x - 1, z + 1] && wallMap[x - 1, z + 1]));
+
+            //if ( )
+
+            {
+
+            }
+        }
+
+    } */
 }
