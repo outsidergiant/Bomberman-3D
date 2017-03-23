b446ac0ded32936bf947cbaee6de4c3a83f9c1e3
Paul <sirdjpavline@yandex.ru>
2017-03-24 00:57:44 +0300

    Task 5.......

--- Bomberman 3D/Assets/Scripts/PlayerController.cs
+++ Bomberman 3D/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private BrickWallView brickWallView;
+    public float speed;
+    public Rigidbody rigidBody;
 	// Use this for initialization
 	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
         brickWallView = (BrickWallView)FindObjectOfType(typeof(BrickWallView));
         int x = -1;
         int z = -1;
@@ -15,11 +18,10 @@ public class PlayerController : MonoBehaviour {
             x = (int)Random.Range(1, GlobalParameters.numberTilesX - 1);
             z = (int)Random.Range(1, GlobalParameters.numberTilesZ - 1);
         } while (x % 2 == 0 || z % 2 == 0 || brickWallView.wallMap[x, z]);
-        transform.position = new Vector3(x, GlobalParameters.Y, z);
+        transform.position = new Vector3(x, GlobalParameters.Y * 2, z);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        
+	void FixedUpdate () {
+        rigidBody.velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * 10;
 	}
 }
